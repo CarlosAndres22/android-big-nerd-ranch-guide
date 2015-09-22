347e9706d23269e6f498d571de073e0db3f0b969
Muhammad Farag <MuhammadFarag@Gmail.com>
2015-09-22 09:18:26 +0200

    fix: rotating answer screen does not lose the status of the user (viewed the answer or not)

--- geoquiz/app/src/main/java/com/mfarag/geoquiz/AnswerActivity.java
+++ geoquiz/app/src/main/java/com/mfarag/geoquiz/AnswerActivity.java
@@ -13,21 +13,36 @@ import android.widget.TextView;
 public class AnswerActivity extends AppCompatActivity {
     private static final String EXTRA_ANSWER_IS_TRUE = "com.mfarag.geoquiz.answer_is_true";
     public static final String EXTRA_ANSWER_SHOWN = "com.mfarag.geoquiz.answer_shown";
+    public static final String KEY_ANSWER_SHOWN = "answer shown";
+    public static boolean sAnswerWasShown = false;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_answer);
+
+        if (savedInstanceState != null) {
+            sAnswerWasShown = savedInstanceState.getBoolean(KEY_ANSWER_SHOWN, false);
+            setResult(Activity.RESULT_OK, new Intent().putExtra(EXTRA_ANSWER_SHOWN, sAnswerWasShown));
+        }
+
         final boolean answerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
 
         findViewById(R.id.button_answer_show_answer).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                sAnswerWasShown = true;
                 ((TextView) findViewById(R.id.answer_text)).setText(String.valueOf(answerIsTrue));
-                setResult(Activity.RESULT_OK, new Intent().putExtra(EXTRA_ANSWER_SHOWN, true));
+                setResult(Activity.RESULT_OK, new Intent().putExtra(EXTRA_ANSWER_SHOWN, sAnswerWasShown));
             }
         });
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putBoolean(KEY_ANSWER_SHOWN,sAnswerWasShown);
     }
 
     @Override
