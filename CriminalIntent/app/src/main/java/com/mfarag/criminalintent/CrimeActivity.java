92402f13caa9360b4910b9c20c4ba5aa6d93ab21
Muhammad Farag <MuhammadFarag@Gmail.com>
2015-09-23 20:05:18 +0200

    refactor: rearrange code for better reusability

--- CriminalIntent/app/src/main/java/com/mfarag/criminalintent/CrimeActivity.java
+++ CriminalIntent/app/src/main/java/com/mfarag/criminalintent/CrimeActivity.java
@@ -1,29 +1,14 @@
 package com.mfarag.criminalintent;
 
-import android.app.Activity;
-import android.content.UriMatcher;
-import android.net.Uri;
-import android.os.Build;
-import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
 
-public class CrimeActivity extends FragmentActivity {
+public class CrimeActivity extends SingleFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_crime);
-
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        Fragment fragment = fragmentManager.findFragmentById(R.id.fragment_container);
-        if (fragment == null) {
-            fragment = new CrimeFragment();
-            fragmentManager.beginTransaction()
-                    .add(R.id.fragment_container, fragment)
-                    .commit();
-        }
+    @NonNull
+    protected Fragment createFragment() {
+        return new CrimeFragment();
     }
 
 }
---
+++ CriminalIntent/app/src/main/java/com/mfarag/criminalintent/SingleFragmentActivity.java
@@ -0,0 +1,30 @@
+package com.mfarag.criminalintent;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+
+/**
+ * Created by muhammadfarag on 9/23/15.
+ */
+public abstract class SingleFragmentActivity extends FragmentActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragment = fragmentManager.findFragmentById(R.id.fragment_container);
+        if (fragment == null) {
+            fragment = createFragment();
+            fragmentManager.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
+    }
+
+    @NonNull
+    protected abstract Fragment createFragment();
+}
